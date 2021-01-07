07159798d3b904cba0c6190d3aae0c4739002bbf
fanlushuai <fanlushuai@github.com>
2021-01-07 19:11:19 +0800

    添加注释，知道config文件的位置用法。领会go代码和文件分离的思维。

--- config/config.go
+++ config/config.go
@@ -29,6 +29,8 @@ var Config Jd
 
 func init() {
 	//关于配置文件的路径问题。难搞。哈哈哈
+	//也许配置文件和代码分离的设计。也是go特意考虑的，代码不管在哪里运行。都能做到代码和文件分离。
+	//所以，你懂得，在哪里运行，请把config.yml文件拷贝到哪里
 	viper.AddConfigPath(".")
 	viper.SetConfigName("config")
 	viper.SetConfigType("yml")
